f49aaddaabc2d79be2a66442fb37ca64486b5473
Shiihon <144337411+Shiihon@users.noreply.github.com>
2024-10-22 22:08:06 +0200

    saving

--- src/main/java/app/DTOs/CountryDTO.java
+++ src/main/java/app/DTOs/CountryDTO.java
@@ -23,7 +23,7 @@ public class CountryDTO {
     private String OfficialLanguage;
     private String nationalAnimal;
     private List<NationalDish> nationalDishes;
-    private List<City> cities;
+    private List<Sight> sightseeingSpots;
 
     public CountryDTO(Country country) {
         this.id = country.getId();
@@ -34,7 +34,7 @@ public class CountryDTO {
         this.OfficialLanguage = country.getOfficialLanguage();
         this.nationalAnimal = country.getNationalAnimal();
         this.nationalDishes = country.getNationalDishes().stream().map(NationalDishDTO::new).collect(Collectors.toList());
-        this.cities = country.getCities().stream().map(CityDTO::new).collect(Collectors.toList());
+        this.sightseeingSpots = country.getSightseeingSpots().stream().map(SightsDTO::new).collect(Collectors.toList());
     }
 
     @JsonIgnore
@@ -47,9 +47,9 @@ public class CountryDTO {
                 new ArrayList<>();
 
         // Convert Cities
-        List<City> cityEntities = (this.cities != null) ?
-                this.cities.stream()
-                        .map(CityDTO::getAsEntity)
+        List<Sights> sightEntities = (this.sightseeingSpots != null) ?
+                this.sightseeingSpots.stream()
+                        .map(SightsDTO::getAsEntity)
                         .collect(Collectors.toList()) :
                 new ArrayList<>();
 
@@ -61,7 +61,7 @@ public class CountryDTO {
                 .currency(currency)
                 .nationalAnimal(nationalAnimal)
                 .nationalDishes(nationalDishEntities)
-                .cities(cityEntities)
+                .SightseeingSpots(sightEntities)
                 .build();
     }
 }
