13c41ce7ae8db385bab1581f9ffe9f17d29b56cf
Ømer <cph-oo203@cphbusiness.dk>
2024-10-22 13:00:00 +0200

    added workflow, dockerfile and dummyTest

---
+++ src/test/java/app/MainTest.java
@@ -0,0 +1,20 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void add() {
+        Main main = new Main();
+        assertEquals(5, main.add(2, 3));
+    }
+
+    @Test
+    void add2() {
+        Main main = new Main();
+        assertEquals(10, main.add(6, 5));
+    }
+}
